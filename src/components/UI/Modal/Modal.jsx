8c7163ffb55540a081b1a68cb145001d25c545e2
pvslvch <polina.vasilevichh@gmail.com>
2025-06-23 12:23:37 +0200

    refactor: refactor Modal component

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -3,19 +3,25 @@ import { createPortal } from "react-dom";
 
 import cls from "./Modal.module.css";
 
-export const Modal = ({ isOpen, children, onClose }) => {
+export const Modal = ({ isOpen, children, className = "", onClose }) => {
   const dialogRef = useRef();
 
   useEffect(() => {
+    const modal = dialogRef.current;
+
     if (isOpen) {
-      dialogRef.current.showModal();
-    } else {
-      dialogRef.current.close();
+      modal.showModal();
     }
+
+    return () => modal.close();
   }, [isOpen]);
 
   return createPortal(
-    <dialog ref={dialogRef} onClose={onClose} className={cls.modal}>
+    <dialog
+      ref={dialogRef}
+      onClose={onClose}
+      className={`${cls.modal} ${className}`}
+    >
       <button className={cls.closeBtn} onClick={onClose}>
         x
       </button>
