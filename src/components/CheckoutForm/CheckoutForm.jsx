e75417832199f01bbcbcf921f9b0f59eaf4d685b
pvslvch <polina.vasilevichh@gmail.com>
2025-06-24 10:41:10 +0200

    feat: implement CheckoutForm component

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,7 +1,7 @@
 import { use, useActionState } from "react";
 
-import { ProductsContext } from "../../store/products-context";
 import ShoppingCartContext from "../../store/shopping-cart-context";
+import UserProgressContext from "../../store/user-progress-context";
 
 import { SubmitButton } from "../UI/SubmitButton";
 import { Input } from "../UI/Input";
@@ -11,46 +11,49 @@ import { isEmail, isNotEmpty } from "../../utils/validation";
 import { getTotalPrice } from "../../utils/getTotalPrice";
 import { currencyFormatter } from "../../utils/currencyFormatter";
 
-import cls from "./CheckoutForm.module.css";
-import UserProgressContext from "../../store/user-progress-context";
 import { USER_PROGRESS_STATE } from "../../constants";
 
-export const CheckoutForm = ({ onOpenNotificationModal }) => {
+import cls from "./CheckoutForm.module.css";
+import { ProductsContext } from "../../store/products-context";
+
+export const CheckoutForm = () => {
   const { addOrder } = use(ProductsContext);
   const { items, clearCart } = use(ShoppingCartContext);
   const { progress, hideCheckout } = use(UserProgressContext);
 
   const cartTotalPrice = getTotalPrice(items);
 
+  // function handleCloseCheckoutForm() {}
+
   async function checkoutFormAction(_prevFormState, formData) {
     try {
-      // const userData = Object.entries(formData);
+      const userData = Object.fromEntries(formData.entries());
 
-      const fullName = formData.get("fullName");
-      const email = formData.get("email");
-      const street = formData.get("street");
-      const postalCode = formData.get("postalCode");
-      const city = formData.get("city");
+      // const fullName = formData.get("full-name");
+      // const email = formData.get("email");
+      // const street = formData.get("street");
+      // const postalCode = formData.get("postal-code");
+      // const city = formData.get("city");
 
       let errors = {};
 
-      if (!isNotEmpty(fullName)) {
+      if (!isNotEmpty(userData.name)) {
         errors.fullName = "Please enter your name.";
       }
 
-      if (!isNotEmpty(email) || !isEmail(email)) {
+      if (!isNotEmpty(userData.email) || !isEmail(userData.email)) {
         errors.email = "Please enter valid email.";
       }
 
-      if (!isNotEmpty(street)) {
+      if (!isNotEmpty(userData.street)) {
         errors.street = "Please enter your street.";
       }
 
-      if (!isNotEmpty(postalCode)) {
+      if (!isNotEmpty(userData["postal-code"])) {
         errors.postalCode = "Please enter your postal code.";
       }
 
-      if (!isNotEmpty(city)) {
+      if (!isNotEmpty(userData.city)) {
         errors.city = "Please enter your city.";
       }
 
@@ -58,11 +61,7 @@ export const CheckoutForm = ({ onOpenNotificationModal }) => {
         return {
           errors,
           enteredValues: {
-            fullName,
-            email,
-            street,
-            postalCode,
-            city,
+            ...userData,
           },
         };
       }
@@ -70,20 +69,14 @@ export const CheckoutForm = ({ onOpenNotificationModal }) => {
       await addOrder({
         order: {
           items: items,
-          customer: {
-            name: fullName,
-            email,
-            street,
-            ["postal-code"]: postalCode,
-            city,
-          },
+          customer: userData,
         },
       });
 
-      onOpenNotificationModal(
-        "Your order was submitted successfully.",
-        "success"
-      );
+      // onOpenNotificationModal(
+      //   "Your order was submitted successfully.",
+      //   "success"
+      // );
 
       clearCart();
 
@@ -91,7 +84,7 @@ export const CheckoutForm = ({ onOpenNotificationModal }) => {
     } catch (error) {
       console.error(error.message);
 
-      onOpenNotificationModal(error.message, "error");
+      // onOpenNotificationModal(error.message, "error");
 
       return {};
     }
@@ -112,7 +105,7 @@ export const CheckoutForm = ({ onOpenNotificationModal }) => {
           Total price: {currencyFormatter.format(cartTotalPrice)}
         </p>
         <Input
-          id="full-name"
+          id="name"
           label="Full Name"
           defaultValue={formState.enteredValues?.fullName}
           error={formState.errors?.fullName}
